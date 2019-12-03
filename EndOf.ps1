999bb5f69668db0947b91404b69b8b888ec34d5e
Steven Retz <sretz@innovoft.com>
2019-12-02 23:29:33 -0300

    Set-Variable -Name location -Value location -Scope Global

--- EndOf.ps1
+++ EndOf.ps1
@@ -1,4 +1,4 @@
-Set-Variable -Name "location" -Value "" -Scope Global
+Set-Variable -Name "location" -Value "location" -Scope Global
 Set-Variable -Name "threads" -Value 1 -Scope Global
 
 (Get-Process -Id $pid).priorityclass = "Idle"
