346b982b63a07454f9f748d64fb3b76417a2d3b7
Steven Retz <sretz@innovoft.com>
2020-03-29 23:08:57 -0300

    renamed files

--- EndOf.ps1
+++ EndOf.ps1
@@ -4,7 +4,7 @@ $now = [System.DateTime]::UtcNow
 $day = $now.Date.ToString("yyyy-MM-dd")
 $week = $now.Date.AddDays(-$now.DayOfWeek).ToString("yyyy-MM-dd")
 
-if ([System.IO.File]::Exists("EndOfDay.txt") -and $day -eq [System.IO.File]::ReadAllText("EndOfDay.txt"))
+if ([System.IO.File]::Exists("EndOf.Day.txt") -and $day -eq [System.IO.File]::ReadAllText("EndOf.Day.txt"))
 {
     $dayChanged = $false;
 }
@@ -12,7 +12,7 @@ else
 {
     $dayChanged = $true;
 }
-if ([System.IO.File]::Exists("EndOfWeek.txt") -and $week -eq [System.IO.File]::ReadAllText("EndOfWeek.txt"))
+if ([System.IO.File]::Exists("EndOf.Week.txt") -and $week -eq [System.IO.File]::ReadAllText("EndOf.Week.txt"))
 {
     $weekChanged = $false;
 }
@@ -35,10 +35,10 @@ if ($weekChanged)
 
 if ($dayChanged)
 {
-    [System.IO.File]::WriteAllText("EndOfDay.txt", $day)
+    [System.IO.File]::WriteAllText("EndOf.Day.txt", $day)
 }
 
 if ($weekChanged)
 {
-    [System.IO.File]::WriteAllText("EndOfWeek.txt", $week)
+    [System.IO.File]::WriteAllText("EndOf.Week.txt", $week)
 }
